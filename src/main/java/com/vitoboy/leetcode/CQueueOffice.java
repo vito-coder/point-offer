8e6503bf7785ca7552a2e95aa985da2ea2c6a198
Vito <laiwenbin_gd@163.com>
2020-06-19 14:28:37 +0800

    finished queue test

---
+++ src/main/java/com/vitoboy/leetcode/CQueueOffice.java
@@ -0,0 +1,77 @@
+package com.vitoboy.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/19 13:32
+ * @Version: 1.0
+ *
+ * 面试题09. 用两个栈实现队列
+ * 用两个栈实现一个队列。
+ *
+ * 队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ *
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ *
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ *
+ *
+ *
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/solution/mian-shi-ti-09-yong-liang-ge-zhan-shi-xian-dui-l-3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class CQueueOffice {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    int size;
+
+    public CQueueOffice() {
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+        size = 0;
+    }
+
+    public void appendTail(int value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        size++;
+    }
+
+    public int deleteHead() {
+        if (size == 0) {
+            return -1;
+        }
+        size--;
+        return stack1.pop();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/vitoboy/leetcode/CQueueUpdate.java
@@ -0,0 +1,73 @@
+package com.vitoboy.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/19 13:32
+ * @Version: 1.0
+ *
+ * 面试题09. 用两个栈实现队列
+ * 用两个栈实现一个队列。
+ *
+ * 队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ *
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ *
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ */
+public class CQueueUpdate {
+
+    // 入栈用的
+    private Stack<Integer> input;
+    // 出栈用的
+    private Stack<Integer> output;
+
+    public CQueueUpdate() {
+        this.input = new Stack<>();
+        this.output = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        // 直接入栈
+        input.push(value);
+    }
+
+    public int deleteHead() {
+        Integer result = -1;
+        if (input.size() > 1) {
+            while (input.size() > 1) {
+                output.push(input.pop());
+            }
+            result = input.pop();
+        } else if (input.size() == 1) {
+            return input.pop();
+        }
+        if (output.size() > 0) {
+            while (output.size() > 0) {
+                input.push(output.pop());
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
