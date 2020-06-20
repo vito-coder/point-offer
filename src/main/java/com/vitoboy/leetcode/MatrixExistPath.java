5262f3d1c0cbd153159afe32b4292642459ee458
Vito <laiwenbin_gd@163.com>
2020-06-21 01:32:51 +0800

    implement official solution of question 12 of point offer

--- src/main/java/com/vitoboy/leetcode/MatrixExistPath.java
+++ src/main/java/com/vitoboy/leetcode/MatrixExistPath.java
@@ -1,7 +1,5 @@
 package com.vitoboy.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -45,13 +43,13 @@ public class MatrixExistPath {
         String word = "ABCCED";
         char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
         String word1 = "abcd";
-        char[][] board1 = new char[][]{{'a', 'b'},{ 'c', 'd'}};
+        char[][] board1 = new char[][]{{'a', 'b'}, {'c', 'd'}};
         String word2 = "ba";
         char[][] board2 = new char[][]{{'a', 'b'}};
-        System.out.println(exist(board, word));
+        System.out.println(existUpdate(board, word));
 
-        System.out.println(exist(board1, word1));
-        System.out.println(exist(board2, word2));
+        System.out.println(existUpdate(board1, word1));
+        System.out.println(existUpdate(board2, word2));
 
     }
 
@@ -92,7 +90,7 @@ public class MatrixExistPath {
                     PathPoint point = new PathPoint(i, j, -1, -1, chwd[index], index);
                     int[][] checkPoint = new int[board.length][board[0].length];
                     checkPoint[i][j] = 1;
-                    while (index < word.length()  && point != null) {
+                    while (index < word.length() && point != null) {
                         PathPoint pathPoint = findNextPoint(point, board, checkPoint, chwd);
                         if (pathPoint != null) {
                             index++;
@@ -134,7 +132,7 @@ public class MatrixExistPath {
                 if (prePoint.cow > 0 && checkPoint[prePoint.row][prePoint.cow - 1] < 1 && prePoint.preWay != 2) {
                     if (point.value == board[prePoint.row][prePoint.cow - 1]) {
                         point.row = prePoint.row;
-                        point.cow = prePoint.cow-1;
+                        point.cow = prePoint.cow - 1;
                         prePoint.way = 0;
                         point.preWay = 0;
                         break;
@@ -167,7 +165,7 @@ public class MatrixExistPath {
                 if (prePoint.row < board.length - 1 && checkPoint[prePoint.row + 1][prePoint.cow] < 1 && prePoint.preWay != 1) {
                     if (point.value == board[prePoint.row + 1][prePoint.cow]) {
                         point.row = prePoint.row + 1;
-                        point.cow = prePoint.cow ;
+                        point.cow = prePoint.cow;
                         prePoint.way = 3;
                         point.preWay = 3;
                         break;
@@ -177,6 +175,30 @@ public class MatrixExistPath {
                 return null;
         }
         return point;
+    }
+
+
+    public static boolean existUpdate(char[][] board, String word) {
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, chars, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != words[k]) return false;
+        if (k == words.length - 1) return true;
+        char temp = board[i][j];
+        board[i][j]='/';
+        boolean result = dfs(board, words, i - 1, j, k + 1)
+                || dfs(board, words, i, j - 1, k + 1)
+                || dfs(board, words, i + 1, j, k + 1)
+                || dfs(board, words, i, j + 1, k + 1);
+        board[i][j]=temp;
+        return result;
     }
 
 }
