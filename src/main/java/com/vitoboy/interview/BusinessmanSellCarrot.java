1b9f2f6c1ee590db9d10ea2105d92e11f52e36ab
Vito <laiwenbin_gd@163.com>
2020-06-24 04:35:40 +0800

    add businessman sell carrot

---
+++ src/main/java/com/vitoboy/interview/BusinessmanSellCarrot.java
@@ -0,0 +1,18 @@
+package com.vitoboy.interview;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/24 04:34
+ * @Version: 1.0
+ *
+ * A城一个商人有一头驴子和3000根胡萝卜.
+ * 要将萝卜拉到1000公里外的B城去卖，只能用驴子驮。
+ * 已知驴子一次性可驮1000根胡萝卜,但每走一公里要吃掉一根胡萝卜.
+ * 问商人共可卖出多少胡萝卜?
+ */
+public class BusinessmanSellCarrot {
+
+    public static void main(String[] args) {
+
+    }
+}
