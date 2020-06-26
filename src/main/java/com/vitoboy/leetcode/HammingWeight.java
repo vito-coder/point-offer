2f58be5e61685d39592f021b2d7f5727de3ba784
Vito <laiwenbin_gd@163.com>
2020-06-26 16:27:45 +0800

    添加了计算一整数的二进制的1的个数, 自己的实现, 官方等的实现

---
+++ src/main/java/com/vitoboy/leetcode/HammingWeight.java
@@ -0,0 +1,93 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/26 16:12
+ * @Version: 1.0
+ *
+ * 剑指 Offer 15. 二进制中1的个数
+ * 请实现一个函数，输入一个整数，输出该数二进制表示中 1 的个数。
+ * 例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+ *
+ * 示例 1：
+ *
+ * 输入：00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+ * 示例 2：
+ *
+ * 输入：00000000000000000000000010000000
+ * 输出：1
+ * 解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
+ * 示例 3：
+ *
+ * 输入：11111111111111111111111111111101
+ * 输出：31
+ * 解释：输入的二进制串 11111111111111111111111111111101 中，共有 31 位为 '1'。
+ *
+ */
+public class HammingWeight {
+
+    public static void main(String[] args) {
+        int num = 10;
+        Integer n = new Integer(10);
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toBinaryString(-10));
+        System.out.println(hammingWeight(-10));
+
+    }
+
+    public static int hammingWeight(int n) {
+        String binaryStr = Integer.toBinaryString(n);
+        int sum = 0;
+        char[] chars = binaryStr.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '1'){
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    /**
+     * 作者：jyd
+     *     链接：https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/solution/mian-shi-ti-15-er-jin-zhi-zhong-1de-ge-shu-wei-yun/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public static int fastHammingWeight(int n) {
+        int res = 0;
+        while(n != 0) {
+            res += n & 1;
+            n >>>= 1;
+        }
+        return res;
+    }
+
+
+    /**
+     * 作者：jyd
+     *     链接：https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/solution/mian-shi-ti-15-er-jin-zhi-zhong-1de-ge-shu-wei-yun/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public static int mostFastHammingWeight(int n) {
+        int res = 0;
+        while(n != 0) {
+            res++;
+            n &= n - 1;
+        }
+        return res;
+    }
+
+
+
+
+}
