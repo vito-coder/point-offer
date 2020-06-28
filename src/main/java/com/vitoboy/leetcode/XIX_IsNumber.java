e82cc157434dbb6ae5e29d96d8a037e7ddf192e5
Vito <laiwenbin_gd@163.com>
2020-06-28 19:06:24 +0800

    添加三道题 1.正则表达式匹配 2.表示数值的字符串 3.调整数组顺序使奇数位于偶数前面

---
+++ src/main/java/com/vitoboy/leetcode/XIX_IsNumber.java
@@ -0,0 +1,18 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/28 19:03
+ * @Version: 1.0
+ */
+public class XIX_IsNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isNumber(String s) {
+
+        return false;
+    }
+}
---
+++ src/main/java/com/vitoboy/leetcode/XX_Exchange.java
@@ -0,0 +1,34 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/28 19:04
+ * @Version: 1.0
+ *
+ * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,2,4]
+ * 注：[3,1,2,4] 也是正确的答案之一。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 50000
+ * 1 <= nums[i] <= 10000
+ */
+public class XX_Exchange {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] exchange(int[] nums) {
+
+        return null;
+    }
+}
