1f6bb7a0ab95f2142841c3df7d28ec4af6432dc5
Vito <laiwenbin_gd@163.com>
2020-06-28 18:28:41 +0800

    抄了一下关于大数情况下的剪绳子问题

--- src/main/java/com/vitoboy/leetcode/XIII_CuttingRope_II.java
+++ src/main/java/com/vitoboy/leetcode/XIII_CuttingRope_II.java
@@ -32,10 +32,48 @@ package com.vitoboy.leetcode;
 public class XIII_CuttingRope_II {
     public static void main(String[] args) {
 
+        XIII_CuttingRope_II cuttingRope_ii = new XIII_CuttingRope_II();
+        System.out.println(cuttingRope_ii.cuttingRope(120));
     }
 
     public int cuttingRope(int n) {
+        if (n <= 3 ) return n-1;
+        long res = 1;
+        while (n > 4) {
+            res *= 3;
+            res = res % 1000000007;
+            n -= 3;
+        }
+        return (int) ((res * n) % 1000000007);
+    }
 
-        return 0;
+    /**
+     *
+     作者：chuang-bian-gu-shi
+     链接：https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/solution/ctan-xin-0ms-by-chuang-bian-gu-shi/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param n
+     * @return
+     */
+    //n >= 5 2*(n-2) > n   3*(n-3) > n  且3*(n-3) >= 2*(n-2)
+    //n = 4 2 * 2 > 1 * 3
+    //2和3不能再分了  分了就变小了 且3优于2
+    public int cuttingRopeNiceAnswer(int n) {
+        if(n <= 3) return n-1;
+        long rs = 1;
+        while (n > 4) {
+            //3最优
+            rs *= 3;
+            rs %= 1000000007;
+            n -= 3;
+        }
+        //n = 1,2,3,4,5,6,7,8,9,10,11,12  减了n个3之后
+        //n = 1,2,3,4,2,3,4,2,3,4, 2, 3
+        //循环结束 只剩下1, 2 ,3,4   *1等于没有
+        //2，3不能再分了
+        //4 可以分成1 * 3  2 * 2,所以还是4最优
+        //所以 剩下的1 2 3 4 都不能再分了
+        return (int) ((rs * n) % 1000000007);
     }
 }
