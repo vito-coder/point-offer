869eda50c02eaa6ca258bd13ec4c9e13313b89fb
Vito <laiwenbin_gd@163.com>
2020-07-01 00:05:20 +0800

    添加题目'序列化二叉树';

---
+++ src/main/java/com/vitoboy/leetcode/XXXVII_Codec.java
@@ -0,0 +1,45 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/1 00:03
+ * @Version: 1.0
+ *
+ * 剑指 Offer 37. 序列化二叉树
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ *
+ * 示例:
+ *
+ * 你可以将以下二叉树：
+ *
+ *     1
+ *    / \
+ *   2   3
+ *      / \
+ *     4   5
+ *
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ *
+ */
+public class XXXVII_Codec {
+    public static void main(String[] args) {
+        // todo
+
+    }
+
+}
+
+class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        // todo
+        return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        // todo
+        return null;
+    }
+}
