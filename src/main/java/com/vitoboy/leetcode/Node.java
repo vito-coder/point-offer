89834ff6c1dc4fc3b2a676017ec22d3a349340ae
Vito <laiwenbin_gd@163.com>
2020-07-01 00:02:49 +0800

    添加题目'二叉搜索树与双向链表';

--- src/main/java/com/vitoboy/leetcode/Node.java
+++ src/main/java/com/vitoboy/leetcode/Node.java
@@ -9,10 +9,20 @@ public class Node {
     int val;
     Node next;
     Node random;
+    Node left;
+    Node right;
+
+    public Node(){}
 
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+    }
+
+    public Node(int val,Node left,Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
