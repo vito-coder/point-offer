80c107fe32b3adface9119459124fe27c775f9dc
Vito <laiwenbin_gd@163.com>
2020-06-30 23:52:39 +0800

    添加题目'二叉树中和为某一值的路径'

---
+++ src/main/java/com/vitoboy/leetcode/XXXIV_PathSum.java
@@ -0,0 +1,44 @@
+package com.vitoboy.leetcode;
+
+import java.util.List;
+
+/**
+ * @Author: vito
+ * @Date: 2020/6/30 23:51
+ * @Version: 1.0
+ *
+ * 剑指 Offer 34. 二叉树中和为某一值的路径
+ * 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ *
+ *
+ *
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \    / \
+ *         7    2  5   1
+ * 返回:
+ *
+ * [
+ *    [5,4,11,2],
+ *    [5,8,4,5]
+ * ]
+ *
+ *
+ * 提示：
+ *
+ * 节点总数 <= 10000
+ */
+public class XXXIV_PathSum {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        return null;
+    }
+}
