5a8543f89d207fe76974127261ecf9e2e8b96c55
Vito <laiwenbin_gd@163.com>
2020-07-01 22:22:20 +0800

    添加题目'把数字翻译成字符串'

---
+++ src/main/java/com/vitoboy/leetcode/XLV_TranslateNum.java
@@ -0,0 +1,35 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/1 22:17
+ * @Version: 1.0
+ *
+ * 剑指 Offer 46. 把数字翻译成字符串
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：
+ * 0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+ * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: 12258
+ * 输出: 5
+ * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ *
+ *
+ * 提示：
+ *
+ * 0 <= num < 2^31
+ */
+public class XLV_TranslateNum {
+    public static void main(String[] args) {
+        // todo
+    }
+
+    public int translateNum(int num) {
+        // todo
+        return 0;
+    }
+}
