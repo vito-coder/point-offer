e64a9630a991cb4b46fb12bd0da1ee8c1e08d045
Vito <laiwenbin_gd@163.com>
2020-07-01 23:48:50 +0800

    添加题目'数组中的逆序对'

---
+++ src/main/java/com/vitoboy/leetcode/L_ReversePairs.java
@@ -0,0 +1,32 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/1 23:46
+ * @Version: 1.0
+ *
+ * 剑指 Offer 51. 数组中的逆序对
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: [7,5,6,4]
+ * 输出: 5
+ *
+ *
+ * 限制：
+ *
+ * 0 <= 数组长度 <= 50000
+ */
+public class L_ReversePairs {
+    public static void main(String[] args) {
+        // todo
+    }
+
+    public int reversePairs(int[] nums) {
+        // todo
+        return 0;
+    }
+}
