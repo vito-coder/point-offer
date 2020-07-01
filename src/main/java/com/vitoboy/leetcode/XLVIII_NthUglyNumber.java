a7f7812414384317641f0805d5680c1d6b3aeae2
Vito <laiwenbin_gd@163.com>
2020-07-01 22:27:21 +0800

    添加题目'丑数'

---
+++ src/main/java/com/vitoboy/leetcode/XLVIII_NthUglyNumber.java
@@ -0,0 +1,32 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/1 22:26
+ * @Version: 1.0
+ *
+ * 剑指 Offer 49. 丑数
+ * 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ *
+ *
+ *
+ * 示例:
+ *
+ * 输入: n = 10
+ * 输出: 12
+ * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+ * 说明:
+ *
+ * 1 是丑数。
+ * n 不超过1690。
+ */
+public class XLVIII_NthUglyNumber {
+    public static void main(String[] args) {
+        // todo
+    }
+
+    public int nthUglyNumber(int n) {
+        // todo
+        return 0;
+    }
+}
