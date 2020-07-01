21f652e7b80d87bbac089921763e86f4e687303c
Vito <laiwenbin_gd@163.com>
2020-07-01 12:48:25 +0800

    1.自己实现 2.使用java类库优化 3.官方推荐实现1,2

--- src/main/java/com/vitoboy/leetcode/XL_GetLeastNumbers.java
+++ src/main/java/com/vitoboy/leetcode/XL_GetLeastNumbers.java
@@ -9,7 +9,6 @@ import java.util.Queue;
  * @Author: vito
  * @Date: 2020/7/1 00:49
  * @Version: 1.0
- * <p>
  * 剑指 Offer 40. 最小的k个数
  * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
  * <p>
