2db0bbb9a0f67a0c010fa7fd830e3550c5fd0552
Vito <laiwenbin_gd@163.com>
2020-07-02 12:11:31 +0800

    添加题目: '二叉搜索树的第k大节点';

---
+++ src/main/java/com/vitoboy/leetcode/LIV_KthLargest.java
@@ -0,0 +1,50 @@
+package com.vitoboy.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/2 11:55
+ * @Version: 1.0
+ *
+ * 剑指 Offer 54. 二叉搜索树的第k大节点
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * 输出: 4
+ *
+ *
+ * 限制：
+ *
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ */
+public class LIV_KthLargest {
+    public static void main(String[] args) {
+        // todo
+    }
+
+    public int kthLargest(TreeNode root, int k) {
+        // todo
+        return 0;
+    }
+}
