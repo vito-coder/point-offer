d2b92b4830048859fe35f04a71700aa930354119
Vito <laiwenbin_gd@163.com>
2020-07-15 17:59:21 +0800

    添加题目: 构建乘积数组

---
+++ src/main/java/com/vitoboy/leetcode/LXXI_ConstructArr.java
@@ -0,0 +1,39 @@
+package com.vitoboy.leetcode;
+
+/**
+ * @Author: vito
+ * @Date: 2020/7/15 17:29
+ * @Version: 1.0
+ *
+ * 剑指 Offer 66. 构建乘积数组
+ * 给定一个数组 A[0,1,…,n-1]，
+ * 请构建一个数组 B[0,1,…,n-1]，
+ * 其中 B 中的元素 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。
+ * 不能使用除法。
+ *
+ *
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,4,5]
+ * 输出: [120,60,40,30,24]
+ *
+ *
+ * 提示：
+ *
+ * 所有元素乘积之和不会溢出 32 位整数
+ * a.length <= 100000
+ *
+ */
+public class LXXI_ConstructArr {
+    public static void main(String[] args) {
+        LXXI_ConstructArr constructArr = new LXXI_ConstructArr();
+        // todo
+    }
+
+    public int[] constructArr(int[] a) {
+
+        // todo
+        return null;
+    }
+}
