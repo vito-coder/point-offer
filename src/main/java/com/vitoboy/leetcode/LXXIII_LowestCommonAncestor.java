4e72d555c6f7613f64807c0ae1e0b622d7a25e4c
Vito <laiwenbin_gd@163.com>
2020-07-27 10:44:23 +0800

    添加官方推荐算法, 解决问题二叉搜索树的最近公共祖先"

--- src/main/java/com/vitoboy/leetcode/LXXIII_LowestCommonAncestor.java
+++ src/main/java/com/vitoboy/leetcode/LXXIII_LowestCommonAncestor.java
@@ -52,6 +52,28 @@ public class LXXIII_LowestCommonAncestor {
         System.out.println(commonAncestor.lowestCommonAncestor(root, node2, node4).val);
     }
 
+    /**
+     *
+     * 自己实现的, 但速度太慢了
+     *
+     * 方法二：递归
+     * 递推工作：
+     * 当 p, qp,q 都在 rootroot 的 右子树 中，则开启递归 root.rightroot.right 并返回；
+     * 否则，当 p, qp,q 都在 rootroot 的 左子树 中，则开启递归 root.leftroot.left 并返回；
+     * 返回值： 最近公共祖先 rootroot 。
+     * 复杂度分析：
+     * 时间复杂度 O(N)O(N) ： 其中 NN 为二叉树节点数；每循环一轮排除一层，二叉搜索树的层数最小为 \log NlogN （满二叉树），最大为 NN （退化为链表）。
+     * 空间复杂度 O(N)O(N) ： 最差情况下，即树退化为链表时，递归深度达到树的层数 NN 。
+     *
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-i-er-cha-sou-suo-shu-de-zui-jin-g-7/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // 1. p , q < root
         // 2. p, q > root
@@ -61,4 +83,53 @@ public class LXXIII_LowestCommonAncestor {
         if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
         return root;
     }
+
+
+    /**
+     * 方法一：迭代
+     * 循环搜索： 当节点 rootroot 为空时跳出；
+     * 当 p, qp,q 都在 rootroot 的 右子树 中，则遍历至 root.rightroot.right ；
+     * 否则，当 p, qp,q 都在 rootroot 的 左子树 中，则遍历至 root.leftroot.left ；
+     * 否则，说明找到了 最近公共祖先 ，跳出。
+     * 返回值： 最近公共祖先 rootroot 。
+     * 复杂度分析：
+     * 时间复杂度 O(N)O(N) ： 其中 NN 为二叉树节点数；每循环一轮排除一层，二叉搜索树的层数最小为 \log NlogN （满二叉树），最大为 NN （退化为链表）。
+     * 空间复杂度 O(1)O(1) ： 使用常数大小的额外空间。
+     *
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-i-er-cha-sou-suo-shu-de-zui-jin-g-7/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestorOfficial(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null) {
+            if(root.val < p.val && root.val < q.val) // p,q 都在 root 的右子树中
+                root = root.right; // 遍历至右子节点
+            else if(root.val > p.val && root.val > q.val) // p,q 都在 root 的左子树中
+                root = root.left; // 遍历至左子节点
+            else break;
+        }
+        return root;
+    }
+
+
+    public TreeNode lowestCommonAncestorOfficialUpdate(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > q.val) { // 保证 p < q;
+            TreeNode temp = p;
+            p = q;
+            q = temp;
+        }
+        while (root != null) {
+            if (root.val < p.val) root = root.right;
+            else if (root.val > q.val) root = root.left;
+            else break;
+        }
+        return root;
+    }
+
 }
