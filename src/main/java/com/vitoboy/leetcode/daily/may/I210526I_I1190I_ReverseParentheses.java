f04b925e27ecc7437b5978cad596a32530e31f9f
Vito <laiwenbin_gd@163.com>
2021-05-26 17:09:07 +0800

    - V1.2.15 - 记录官方题解: `1190.反转每对括号间的子串`

--- src/main/java/com/vitoboy/leetcode/daily/may/I210526I_I1190I_ReverseParentheses.java
+++ src/main/java/com/vitoboy/leetcode/daily/may/I210526I_I1190I_ReverseParentheses.java
@@ -1,6 +1,6 @@
 package com.vitoboy.leetcode.daily.may;
 
-import javax.swing.plaf.nimbus.AbstractRegionPainter;
+
 import java.util.Stack;
 
 /**
@@ -82,11 +82,33 @@ public class I210526I_I1190I_ReverseParentheses {
      *  2. 单数层深度括号需要反转, 双数层深度不需要反转
      *  3. 反向计算
      *
+     *     作者：LeetCode-Solution
+     *     链接：https://leetcode-cn.com/problems/reverse-substrings-between-each-pair-of-parentheses/solution/fan-zhuan-mei-dui-gua-hao-jian-de-zi-chu-gwpv/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     *  			解答成功:
+     * 				执行耗时:1 ms,击败了98.96% 的Java用户
+     * 				内存消耗:36.6 MB,击败了81.35% 的Java用户
+     *
      * @param s
      * @return
      */
     public String reverseParentheses(String s) {
-
-        return null;
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(sb.toString());
+                sb.setLength(0);
+            } else if (c == ')') {
+                sb.reverse();
+                sb.insert(0, stack.pop());
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
