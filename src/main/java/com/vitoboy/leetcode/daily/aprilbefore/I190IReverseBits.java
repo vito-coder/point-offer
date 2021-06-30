be9ee8bce4142c5b1305a7a59aeb06d19fa2755b
Vito <laiwenbin_gd@163.com>
2021-06-30 17:02:58 +0800

    - V1.2.27 - 自己实现: `190.颠倒二进制位`

--- src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I190IReverseBits.java
+++ src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I190IReverseBits.java
@@ -12,42 +12,18 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  *还是无符号的，其内部的二进制表示形式都是相同的。 
  * 在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在上面的 示例 2 中，输入表示有符号整数 -3，输出表示有符号整数 -10737418
  *25。 
- * 
  *
- * 
  *
  * 进阶: 
  *如果多次调用这个函数，你将如何优化你的算法？ 
  *
- * 
- *
- * 示例 1： 
- *
- * 
- *输入: 00000010100101000001111010011100
- *输出: 00111001011110000010100101000000
- *解释: 输入的二进制串 00000010100101000001111010011100 表示无符号整数 43261596，
- *     因此返回 964176192，其二进制表示形式为 00111001011110000010100101000000。 
- *
- * 示例 2： 
- *
- * 
- *输入：11111111111111111111111111111101
- *输出：10111111111111111111111111111111
- *解释：输入的二进制串 11111111111111111111111111111101 表示无符号整数 4294967293，
- *     因此返回 3221225471 其二进制表示形式为 10111111111111111111111111111111 。 
- *
- * 示例 1： 
- *
- * 
+ * 示例 1：
  *输入：n = 00000010100101000001111010011100
  *输出：964176192 (00111001011110000010100101000000)
  *解释：输入的二进制串 00000010100101000001111010011100 表示无符号整数 43261596，
  *     因此返回 964176192，其二进制表示形式为 00111001011110000010100101000000。 
  *
- * 示例 2： 
- *
- * 
+ * 示例 2：
  *输入：n = 11111111111111111111111111111101
  *输出：3221225471 (10111111111111111111111111111111)
  *解释：输入的二进制串 11111111111111111111111111111101 表示无符号整数 4294967293，
@@ -55,9 +31,7 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  *
  * 
  *
- * 提示： 
- *
- * 
+ * 提示：
  * 输入是一个长度为 32 的二进制字符串 
  * 
  * Related Topics 位运算 
@@ -69,10 +43,15 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  * @Version: 1.0
  */
 public class I190IReverseBits {
-    // todo 待解决问题
+    // 待解决问题
     public static void main(String[] args) {
-        ReverseBitsInterface reverseBits = new ReverseBits();
-        reverseBits.reverseBits(123);
+        ReverseBitsInterface bits = new ReverseBits();
+        System.out.println(43261596 ^ 43261595);
+        System.out.println(bits.reverseBits(43261596));
+        System.out.println("expect is : 964176192");
+        System.out.println(bits.reverseBits(-3));
+        System.out.println("expect is : -1073741825");
+
 
     }
 
@@ -82,10 +61,25 @@ public class I190IReverseBits {
 
     static class ReverseBits implements ReverseBitsInterface {
 
+        /**
+         * 				解答成功:
+         * 				执行耗时:1 ms,击败了100.00% 的Java用户
+         * 				内存消耗:38.2 MB,击败了43.43% 的Java用户
+         * 时间复杂度: O(1)
+         * 空间复杂度: O(1)
+         *
+         * @param n
+         * @return
+         */
         @Override
         public int reverseBits(int n) {
-
-            return 0;
+            int res = 0;
+            for (int i = 0; i < 32; i++) {
+                int bit = n & 1;
+                n = n >>> 1;
+                res = (res << 1) + bit;
+            }
+            return res;
         }
     }
 
