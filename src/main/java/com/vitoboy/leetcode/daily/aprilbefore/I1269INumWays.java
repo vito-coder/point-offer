3fce01559aa87cdff74baedfddf8245c727f0d81
Vito <laiwenbin_gd@163.com>
2021-07-01 11:15:14 +0800

    - V1.2.28 - 自己实现: `74.搜索二维矩阵` - 记录题目: `1269.停在原地的方案数` - 记录题目: `54.螺旋矩阵`

--- src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I1269INumWays.java
+++ src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I1269INumWays.java
@@ -9,11 +9,8 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  *  给你两个整数 steps 和 arrLen ，请你计算并返回：在恰好执行 steps 次操作以后，指针仍然指向索引 0 处的方案数。 
  * 
  *  由于答案可能会很大，请返回方案数 模 10^9 + 7 后的结果。 
- * 
- *  
- * 
- *  示例 1： 
- * 
+ *
+ *  示例 1：
  *  输入：steps = 3, arrLen = 2
  * 输出：4
  * 解释：3 步后，总共有 4 种不同的方法可以停在索引 0 处。
@@ -23,8 +20,7 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  * 不动，不动，不动
  *  
  * 
- *  示例 2： 
- * 
+ *  示例 2：
  *  输入：steps = 2, arrLen = 4
  * 输出：2
  * 解释：2 步后，总共有 2 种不同的方法可以停在索引 0 处。
@@ -32,17 +28,12 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  * 不动，不动
  *  
  * 
- *  示例 3： 
- * 
+ *  示例 3：
  *  输入：steps = 4, arrLen = 2
  * 输出：8
- *  
+ *
  * 
- *  
- * 
- *  提示： 
- * 
- *  
+ *  提示：
  *  1 <= steps <= 500 
  *  1 <= arrLen <= 10^6 
  *  
@@ -58,5 +49,9 @@ public class I1269INumWays {
     // todo 待解决问题
     public static void main(String[] args) {
         System.out.println("待解决问题");
+    }
+
+    public int numWays(int steps, int arrLen) {
+        return 0;
     }
 }
--- src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I74ISearchMatrix.java
+++ src/main/java/com/vitoboy/leetcode/daily/aprilbefore/I74ISearchMatrix.java
@@ -6,33 +6,21 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  * 
  * 每行中的整数从左到右按升序排列。 
  * 每行的第一个整数大于前一行的最后一个整数。 
- * 
  *
- * 
- *
- * 示例 1： 
- *
- * 
+ * 示例 1：
  *输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
  *输出：true
- * 
  *
- * 示例 2： 
- *
- * 
+ * 示例 2：
  *输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
  *输出：false
  * 
  *
- * 
- *
- * 提示： 
- *
- * 
+ * 提示：
  * m == matrix.length 
  * n == matrix[i].length 
  * 1 <= m, n <= 100 
- * -104 <= matrix[i][j], target <= 104 
+ * -10^4 <= matrix[i][j], target <= 10^4
  * 
  * Related Topics 数组 二分查找 
  * 👍 404 👎 0
@@ -42,9 +30,8 @@ package com.vitoboy.leetcode.daily.aprilbefore;
  * @Version: 1.0
  */
 public class I74ISearchMatrix {
-    // todo 待解决问题--一直未提交, 待确认原因
     public static void main(String[] args) {
-        SearchMatrixInterface searchMatrix = new SearchMatrix();
+        SearchMatrixInterface searchMatrix = new SearchMatrixII();
         int[][] matrix = createTest(1);
         System.out.println("search result is : " + searchMatrix.searchMatrix(matrix, 3));
         System.out.println("expect result is : true");
@@ -137,6 +124,63 @@ public class I74ISearchMatrix {
 
 
 
+    }
+
+
+    /**
+     *
+     * 				解答成功:
+     * 				执行耗时:0 ms,击败了100.00% 的Java用户
+     * 				内存消耗:38.1 MB,击败了15.90% 的Java用户
+     *
+     * 时间复杂度: O(logM X logN)
+     * 空间复杂度: O(1)
+     *
+     */
+    static class SearchMatrixII implements SearchMatrixInterface{
+
+        @Override
+        public boolean searchMatrix(int[][] matrix, int target) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+            int row = matrix.length;
+            if (row == 1) {
+                return binarySearchArray(matrix[0], target);
+            } else {
+                int low = 0, high = row, mid = 0;
+                while (low < high) {
+                    mid = (low+high)/2;
+                    int[] tmp = matrix[mid];
+                    if (tmp[0] == target) return true;
+                    if (tmp[0] > target) {
+                        high = mid;
+                    } else {
+                        if (tmp[tmp.length-1] == target) return true;
+                        if (tmp[tmp.length-1] > target) {
+                            return binarySearchArray(tmp, target);
+                        } else {
+                            low = mid+1;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private boolean binarySearchArray(int[] array, int target) {
+            if (array == null || array.length == 0) return false;
+            int low = 0, high = array.length, mid = 0;
+            while (low < high) {
+                mid = (low+high)/2;
+                if (array[mid] == target) return true;
+                if (array[mid] > target) {
+                    high = mid;
+                } else {
+                    low = mid+1;
+                }
+            }
+            return false;
+        }
     }
     
 }
